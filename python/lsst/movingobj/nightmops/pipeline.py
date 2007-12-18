23cd1da1e9dc23b824d02a2f782cbb3570462630
taxelrod <taxelrod@git.lsstcorp.org>
2007-12-18 19:09:31 +0000

    Added logging to nightmops

--- python/lsst/movingobj/nightmops/pipeline.py
+++ python/lsst/movingobj/nightmops/pipeline.py
@@ -2,6 +2,10 @@ import lsst.dps.Stage
 import lsst.mwi.data as datap
 import lsst.mwi.policy as policy
 import lsst.mwi.utils as mwiu
+from lsst.mwi.logging import Log
+from lsst.mwi.logging import LogRec
+from lsst.mwi.data import DataProperty
+
 import lsst.fw.Core.fwCatalog as fwCat
 import lsst.movingobj.nightmops.ephemeris as eph
 import lsst.movingobj.nightmops.ephemDB as ephDB
@@ -9,6 +13,10 @@ import lsst.movingobj.nightmops.ephemDB as ephDB
 class MopsStage(lsst.dps.Stage.Stage):
 
     #------------------------------------------------------------------------
+    def __init__(self):
+
+        self.mopsLog = Log(Log.getDefaultLog(), "movingobj.stage")
+
     def process(self): 
         """
         Execute the needed processing code for this Stage
@@ -61,6 +69,15 @@ class MopsStage(lsst.dps.Stage.Stage):
         MJDItem = triggerEvent.findUnique('visitTime')
         mjd = MJDItem.getValueDouble()
 
+        ###########
+        #
+        # Log the beginning of Mops stage for this slice
+        #
+        LogRec(self.mopsLog, Log.INFO) \
+                                  <<  "Began mops stage" \
+                                  << DataProperty("visitId", visitId) \
+                                  << DataProperty("MJD", mjd)
+
         # get this Slice's set of potential objects in the FOV
 
         candidateEphems = ephDB.fetchCandidateEphems(ephemDbFromPolicy, sliceId, numSlices, mjd)
@@ -72,7 +89,16 @@ class MopsStage(lsst.dps.Stage.Stage):
         ephPreds = ephDB.selectOrbitsForFOV(candidateEphems, mjd, fovRA, fovDec, fovDiamFromPolicy)
         mwiu.Trace("lsst.movingobj.MopsStage", 3, 'Number of ephems in fov: %d' % len(ephPreds))
 
-        # build a MopsPredVec for our Stage output
+        ###########
+        #
+        # Log the number of predicted ephems
+        #
+        LogRec(self.mopsLog, Log.INFO) \
+                                  <<  "Predicted ephems" \
+                                  << DataProperty("possible objects at this MJD", len(candidateEphems)) \
+                                  << DataProperty("predicted objects in the FOV", len(ephPreds))
+
+         # build a MopsPredVec for our Stage output
         
         mopsPreds = fwCat.MopsPredVec()
 
@@ -93,6 +119,8 @@ class MopsStage(lsst.dps.Stage.Stage):
         self.activeClipboard.put('MopsPreds', mopsPreds)
         
         self.outputQueue.addDataset(self.activeClipboard)
+
+        self.mopsLog.log("Mops stage processing ended")
 
 
 
