b98dea62102efe6a1dead3c90b1f7932ff59aeca
fpierfed <fpierfed@git.lsstcorp.org>
2008-11-27 10:32:35 +0000

    renamed

--- python/lsst/mops/nightmops/Ephemeris.py
+++ python/lsst/mops/nightmops/Ephemeris.py
@@ -8,14 +8,14 @@ ephemeris calculation function.
 
 """
 
-__author__ = "jmyers"
+
 
 class Ephemeris(object):
-    def __init__(self, orbitID, MJD, RA, Dec, Mag, SMAA, SMIA, PA):
+    def __init__(self, orbitId, MJD, RA, Dec, Mag, SMAA, SMIA, PA):
         """
         Ephemeris: A point in the sky of a moving object at some given time
 
-        orbitID: integer, ID of orbit associated with this Ephemeris
+        orbitId: integer, ID of orbit associated with this Ephemeris
         MJD: Mean Julian Date, floating point
         RA: Right Ascension (deg)
         Dec: Declination (deg)
@@ -24,7 +24,7 @@ class Ephemeris(object):
         SMIA: error ellipse semi minor axis (deg)
         PA:  error ellipse position angle
         """
-        self.orbitId = orbitID
+        self.orbitId = orbitId
         self.MJD = MJD
         self.RA = RA
         self.Dec = Dec
