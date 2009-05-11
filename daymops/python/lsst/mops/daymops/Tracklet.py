be42452690d1077b0db5d3d81ebd323dcec8a781
fpierfed <fpierfed@git.lsstcorp.org>
2009-05-11 18:17:51 +0000

    now computing velocity information.

--- daymops/python/lsst/mops/daymops/Tracklet.py
+++ daymops/python/lsst/mops/daymops/Tracklet.py
@@ -2,14 +2,61 @@ from DayMOPSObject import DayMOPSObject
 from DiaSource import DiaSource
 import lsst.daf.persistence as persistence
 
+
+
 class Tracklet(DayMOPSObject):
-    # TODO: compute Tracklet information.
-    
-    def __init__(self, trackletId=None, diaSourceList=[]):
+    def __init__(self, trackletId=None, diaSourceList=None):
         self._trackletId = trackletId
-        self._diaSourceList = None
+        self.setDiaSourceList(diaSourceList)
         return
     
+    def setDiaSourceList(self, diaSourceList):
+        """
+        Set the internal diaSourceList.
+        
+        @param diaSourceList: DiaSourceList instance.
+        """
+        if(not diaSourceList or not len(diaSourceList)):
+            print('Ops: no DiaSources!')
+        
+        self._diaSourceList = diaSourceList
+        
+        # Update velocity data.
+        if(self._diaSourceList):
+            (self._velRa, 
+             self._velDec, 
+             self._velTot) = self._diaSourceList.computeVelocityStats()
+        return
+    
+    
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
     
 
 
