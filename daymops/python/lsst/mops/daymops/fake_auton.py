b46cb4534535a19597571b1ac938f3a3f0ae8b63
fpierfed <fpierfed@git.lsstcorp.org>
2009-07-24 18:42:32 +0000

    initial (used for testing only).

---
+++ daymops/python/lsst/mops/daymops/fake_auton.py
@@ -0,0 +1,94 @@
+import random
+import numpy
+
+
+
+
+def findtracklets(detections, maxv, minobs, maxt, etime):
+    """
+    Just return random associations of detections as a list of the form
+        [[detId1, detId2, ...], ]
+    
+    Input detections are a list of 
+        [id, mjd, ra, dec, mag, obscode, objName, trailLength, trailAngle]
+    
+    @param detections: list of input detections.
+    All other parameters are ignored.
+    """
+    if(len(detections) < 2):
+        return([])
+    
+    diaSourceIds = [d[0] for d in detections]
+    diaSourceMjds = [d[1] for d in detections]
+    
+    # Choose a random number of groups to return, between 0 and 
+    # len(diaSourceIds) / 2 (2 detections per tracklet).
+    n = int(random.random() * len(diaSourceIds) / 2.)
+    
+    # Now create n groups of indeces, each 2 to 5 elements long.
+    # Also make sure that no detections with the same mjds end up in the same 
+    # group.
+    groups = []
+    for i in range(n):
+        tot = int(random.uniform(2, 5))
+        indeces = []
+        mjds = []
+        for x in range(tot):
+            idx = int(random.random()*(len(detections)-1))
+            mjd = diaSourceMjds[idx]
+            while(idx in indeces or mjd in mjds):
+                idx = int(random.random()*(len(detections)-1))
+                mjd = diaSourceMjds[idx]
+            indeces.append(idx)
+            mjds.append(mjd)
+        groups.append([diaSourceIds[idx] for idx in indeces])
+    return(groups)
+
+
+def linktracklets(detections, *args, **kwargs):
+    """
+    Same as above
+    
+    Input detections have the form
+        [(trackletId, detMjd, detRa, detDec, detMag, detObsCode, trackletName),]
+    In the end, several entries share the same tracletId, since each tracklet 
+    has many DiaSources.
+    """
+    # Get the number of unique trackletIds, this is the only thing we care 
+    # about.
+    trackletIds = []
+    for det in detections:
+        _id = det[0]
+        if(_id not in trackletIds):
+            trackletIds.append(_id)
+    
+    # We want at least 3 tracklets per track.
+    if(len(trackletIds) < 3):
+        return([])
+    
+    # Choose a random number of groups to return, between 0 and 
+    # len(trackletIds) / 3 (3 detections per tracklet).
+    n = int(random.random() * len(trackletIds) / 3.)
+    
+    # Now create n groups of indeces, each 3 to 10 elements long.
+    groups = []
+    for i in range(n):
+        tot = int(random.uniform(3, 10))
+        indeces = []
+        for x in range(tot):
+            idx = int(random.random()*(len(trackletIds)-1))
+            while(idx in indeces):
+                idx = int(random.random()*(len(trackletIds)-1))
+            indeces.append(idx)
+        groups.append([trackletIds[idx] for idx in indeces])
+    return(groups)
+
+
+
+
+
+
+
+
+
+
