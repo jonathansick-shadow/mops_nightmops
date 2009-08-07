3e9b4ce8fa8175cfd54ad96ff9c781b857723dc3
fpierfed <fpierfed@git.lsstcorp.org>
2009-08-07 15:41:35 +0000

    initial full implementation

--- daymops/python/lsst/mops/daymops/OrbitManagementStage.py
+++ daymops/python/lsst/mops/daymops/OrbitManagementStage.py
@@ -37,7 +37,10 @@ Output
   1. None/error code?
 """
 from DayMOPSStage import DayMOPSStage
-import attribution
+from MovingObjectList import getAllPreliminaryMovingObjects, updateStatus
+from MovingObject import STATUS as MO_STATUS
+from Tracklet import STATUS as T_STATUS
+import linking
 import lib
 
 import time
@@ -53,30 +56,58 @@ class OrbitManagementStage(DayMOPSStage):
         super(OrbitManagementStage, self).__init__(stageId, policy)
         
         # Read the configuration from policy.
-        self.obsCode = self.getValueFromPolicy('obsCode')
-        self.residThreshold = self.getValueFromPolicy('residThreshold')
-        self.maxSearchRadius = self.getValueFromPolicy('maxSearchRadius')
-        self.minSearchRadius = self.getValueFromPolicy('minSearchRadius')
-        self.maxArclengthForIod = self.getValueFromPolicy('maxArclengthForIod')
-        self.uncertaintySigma = self.getValueFromPolicy('uncertaintySigma')
         self.dbLocStr = self.getValueFromPolicy('database')
         return
     
-    def process(self):
-        # Fetch the clipboard.
-        self.activeClipboard = self.inputQueue.getNextDataset()
+    def preprocess(self):
+        """
+        Fetch all preliminary MovingObject instances from the database. Then, if
+        >1 of these share the same Tracklet, choose the "best" one.
         
-        # What is our rank?
-        i = self.getRank()
-        n = self.getUniverseSize() - 1  # want only real slices
-        self.logIt('INFO', 'Slice ID: %d/%d' %(i, n))
+        Of course the meaning f best here is controversial (at the time of 
+        writing, at least). The decision is made in linking.chooseOrbit().
+        """
+        # Call the superclass preprocess.
+        super(OrbitManagementStage, self).preprocess()
+        self.logIt('INFO', 'Started preprocessing.')
         
+        # Get the prelim. MovingObject instances.
+        movingObjectsIter = getAllPreliminaryMovingObjects(self.obsCode, 
+                                                           shallow=False)
         
-        # Do something!
+        # Now, get the full list of Tracklets and start examining each one to 
+        # see if it is associated to >1 MovingObject. Not the best way to go 
+        # about it since this would be a single SQL statement...
+        # TODO: Implement something better here!
+        trackletToMO = {}       # {tracklet: [MovingObject1,MovingObject2,...]}
+        for mo in movingObjectsIter:
+            tracklets = mo.getTracklets()
+            for t in tracklets:
+                tId = t.getTrackletId()
+                if(trackletToMO.has_key(tId)):
+                    trackletToMO[tId].append(mo)
+                else:
+                    trackletToMO[tId] = [mo, ]
         
+        # And now back again!
+        movingObjects = []
+        for tId in trackletToMO.keys():
+            # Do we have >1 MovingObject?
+            if(len(trackletToMO[tId] <= 1)):
+                # Nope!
+                continue
+            
+            # Yup!
+            mo = linking.chooseOrbit(trackletToMO[tId])
+            if(not mo in movingObjects):
+                # Update the status of this MovingObject and its Tracklets.
+                mo.setStatus(MO_STATUS['NEW'])
+                [t.setStatus(T_STATUS['ATTRIBUTED']) for t in mo.getTracklets()]
+                movingObjects.append(mo)
         
-        # Put the clipboard back.
-        self.outputQueue.addDataset(self.activeClipboard)
+        # Now update the status of the MovingObjects we have chosen and their
+        # Tracklets as well.
+        updateStatus(self.dbLocStr, movingObjects, updateTrackletStatus=True)
         return
     
     
